387782e1deb730843e9b443429e53c78b2070c44
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-16 15:20:30 +1100

    chore: change for blog0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,32 @@
-import { useState } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 
 export default function Home() {
-
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
   const test = "Hello Jest";
-  const onChange = (event:any) => {
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
+  };
+  const onClick = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsClicked(true);
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 3000);
   };
   return (
     <>
-      <input type="text" value={value} onChange={onChange} placeholder={"Type something..."} />
-      <div data-testid="test-val">{value}</div>
-      <div data-testid="my-element">{test}</div>
+      <form onSubmit={onClick}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          placeholder={"Type something..."}
+        />
+        <div data-testid="test-val">{value}</div>
+        <button type={"submit"} disabled={isClicked}>Submit</button>
+        <div data-testid="my-element">{test}</div>
+      </form>
     </>
   );
 }

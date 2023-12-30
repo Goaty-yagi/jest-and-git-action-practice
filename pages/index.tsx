a67e569c2ba91351bfb02ae05c091ec1f7a4cd81
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-12-30 23:19:37 +1100

    test: add onChange and submit test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,27 @@ import { useState, ChangeEvent, FormEvent } from "react";
 
 export default function Home() {
   const [value, setValue] = useState("");
-  const [isClicked, setIsClicked] = useState(false);
+  const [isClicked, setIsClicked] = useState(true);
   const test = "Hello Jest";
   const onChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
+    if (event.target.value) {
+      setIsClicked(false);
+    } else {
+      setIsClicked(true);
+    }
   };
   const onClick = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setIsClicked(true);
-    setTimeout(() => {
-      setIsClicked(false);
-    }, 3000);
+    if (value) {
+      setIsClicked(true);
+      setTimeout(() => {
+        setValue("");
+      }, 3000);
+      console.log("Submited");
+    } else {
+      console.log("Input is empty");
+    }
   };
   return (
     <>
@@ -21,10 +31,13 @@ export default function Home() {
           type="text"
           value={value}
           onChange={onChange}
+          maxLength={10}
           placeholder={"Type something..."}
         />
         <div data-testid="test-val">{value}</div>
-        <button type={"submit"} disabled={isClicked}>Submit</button>
+        <button type={"submit"} disabled={isClicked}>
+          Submit
+        </button>
         <div data-testid="my-element">{test}</div>
       </form>
     </>
